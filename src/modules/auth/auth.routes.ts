bfcfbb325bad172ad4264c1d3cc558637419c77b
baloerrr <akbaradityah444@gmail.com>
2024-09-03 11:06:23 +0700

    feat: add route login and register

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -3,11 +3,13 @@ import passport from '../../common/libs/passport'
 import { getAuthorization, validationMiddleware } from './auth.middleware';
 import AuthController from './auth.controller';
 import { AppPasswordDTO } from './dtos/appPassword.dto';
+import { LoginDTO } from './dtos/login.dto';
+import { RegisterDTO } from './dtos/register.dto';
 
 const router = Router();
 
-// router.post('/register', AuthController.register);
-// router.post('/login', AuthController.login);
+router.post('/register',validationMiddleware(RegisterDTO), AuthController.register);
+router.post('/login', validationMiddleware(LoginDTO), AuthController.login);
 
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
@@ -33,15 +35,20 @@ router.get('/auth/google/callback',
   }
 );
 
-router.get('/logout', (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect('/login');
-    }
-  });
-});
+// router.get('/logout', (req, res) => {
+//   req.logout((err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.redirect('/login');
+//     }
+//   });
+// });
+
+router.get('/logout', (req,res) => {
+  res.clearCookie('authToken'); // Hapus cookie authToken
+    res.redirect('/login'); // Arahkan pengguna ke halaman login
+})
 
 router.post('/save-app-password', getAuthorization, validationMiddleware(AppPasswordDTO), AuthController.saveAppPassword);
 
