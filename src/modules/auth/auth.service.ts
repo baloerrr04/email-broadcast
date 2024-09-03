6ed0bc4ad19ba1627e6d25dcb1a1f11b8f3263ee
baloerrr <akbaradityah444@gmail.com>
2024-09-03 11:06:40 +0700

    fix: update http status code

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,6 +6,7 @@ import jwt from "jsonwebtoken";
 import MailService from "../mailer/mailer.service";
 import { HttpException } from "../../common/exceptions/HttpExceptions";
 import { AppPasswordDTO } from "./dtos/appPassword.dto";
+import httpStatusCodes from "../../common/constants/http-status-codes"
 
 class AuthService {
 
@@ -14,31 +15,31 @@ class AuthService {
     static async register(email: string, password: string): Promise<User> {
         const existingUser = await AuthRepository.findByEmail(email);
 
-        if (existingUser) throw new HttpException(400, "User already exists");
+        if (existingUser) throw new HttpException(httpStatusCodes.BAD_REQUEST, "User already exists");
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = await AuthRepository.createUser({ email, password: hashedPassword });
 
-        if (!newUser) throw new HttpException(500, "User registration failed");
+        if (!newUser) throw new HttpException(httpStatusCodes.INTERNAL_SERVER_ERROR, "User registration failed");
 
         return newUser;
     }
 
     static async login(email: string, password: string): Promise<{ token: string, user: User }> {
         const user = await AuthRepository.findByEmail(email);
-        if (!user) throw new HttpException(401, "Invalid credentials");
+        if (!user) throw new HttpException(httpStatusCodes.UNAUTHORIZED, "Invalid credentials");
 
         const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) throw new HttpException(401, "Invalid credentials");
+        if (!isMatch) throw new HttpException(httpStatusCodes.UNAUTHORIZED, "Invalid credentials");
 
-        if (!AuthService.JWT_SECRET) throw new HttpException(500, "JWT Secret not configured");
+        if (!AuthService.JWT_SECRET) throw new HttpException(httpStatusCodes.INTERNAL_SERVER_ERROR, "JWT Secret not configured");
 
         const token = jwt.sign({ userId: user.id }, AuthService.JWT_SECRET, { expiresIn: '1h' });
         return { token, user };
     }
 
     static async loginGoogle(profile: Profile): Promise<{ token: string, user: User }> {
-        if (!profile.emails || profile.emails.length === 0) throw new HttpException(500, "Google account does not have an email");
+        if (!profile.emails || profile.emails.length === 0) throw new HttpException(httpStatusCodes.INTERNAL_SERVER_ERROR, "Google account does not have an email");
 
         const email = profile.emails[0].value;
         let user = await AuthRepository.findByEmail(email);
@@ -50,10 +51,10 @@ class AuthService {
                 password: bcrypt.hashSync(Math.random().toString(36).substring(7), 10),
             });
 
-            if (!user) throw new HttpException(500, "User creation failed");
+            if (!user) throw new HttpException(httpStatusCodes.INTERNAL_SERVER_ERROR, "User creation failed");
         }
 
-        if (!AuthService.JWT_SECRET) throw new HttpException(500, "JWT Secret not configured");
+        if (!AuthService.JWT_SECRET) throw new HttpException(httpStatusCodes.INTERNAL_SERVER_ERROR, "JWT Secret not configured");
 
         const token = jwt.sign({ userId: user.id }, AuthService.JWT_SECRET, { expiresIn: '1h' });
         return { token, user };
@@ -62,12 +63,12 @@ class AuthService {
     static async saveAppPassword(userId: number, appPassword: AppPasswordDTO): Promise<User> {
         const user = await AuthRepository.findById(userId);
 
-        if (!user) throw new HttpException(404, `User with id ${userId} not found`);
+        if (!user) throw new HttpException(httpStatusCodes.NOT_FOUND, `User with id ${userId} not found`);
 
         // const hashedAppPassword = await bcrypt.hash(appPassword, 10);
         const updatedUser = await AuthRepository.updateUser(userId, appPassword.appPassword);
 
-        if (!updatedUser) throw new HttpException(500, "Saving app password failed");
+        if (!updatedUser) throw new HttpException(httpStatusCodes.INTERNAL_SERVER_ERROR, "Saving app password failed");
 
         const mailService = new MailService()
         mailService.configure(user.email, appPassword.appPassword)
