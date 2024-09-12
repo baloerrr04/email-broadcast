91600f7fa517c738a131cad12fbddab577e0cdc6
baloerrr <akbaradityah444@gmail.com>
2024-09-12 19:06:44 +0700

    fix: modify register dto with Role

--- src/modules/auth/dtos/register.dto.ts
+++ src/modules/auth/dtos/register.dto.ts
@@ -1,3 +1,4 @@
+import { Role } from "@prisma/client";
 import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
 
 export class RegisterDTO {
@@ -7,4 +8,7 @@ export class RegisterDTO {
     @IsNotEmpty({ message: 'Password should not be empty' })
     @MinLength(6, { message: 'Password must be at least 6 characters long' })
     password: string = '';
+
+    @IsNotEmpty({ message: 'Role should not be empty' })
+    role: Role = Role.USER;
 }
