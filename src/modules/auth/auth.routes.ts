09a466253e02cb9418c73d5a8693b64bf5c9a245
baloerrr <akbaradityah444@gmail.com>
2024-09-12 19:05:45 +0700

    feat: add route /add-broadcast

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
-import passport from '../../common/libs/passport'
 import { getAuthorization, validationMiddleware } from './auth.middleware';
 import AuthController from './auth.controller';
-import { AppPasswordDTO } from './dtos/appPassword.dto';
 import { LoginDTO } from './dtos/login.dto';
 import { RegisterDTO } from './dtos/register.dto';
 
@@ -10,47 +8,11 @@ const router = Router();
 
 router.post('/register',validationMiddleware(RegisterDTO), AuthController.register);
 router.post('/login', validationMiddleware(LoginDTO), AuthController.login);
-
-router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-router.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  async (req, res) => {
-    try {
-      const { user, token } = req.user as any;
-
-      res.cookie('authToken', token, { httpOnly: true, maxAge: 3600000 });
-      req.login((user), (err) => {
-        if (err) {
-          console.log(err);
-          res.redirect('/login');
-        } else {
-          res.redirect('/');
-        }
-      });
-    } catch (err) {
-      console.log(err);
-      res.redirect('/login');
-    }
-  }
-);
-
-// router.get('/logout', (req, res) => {
-//   req.logout((err) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       res.redirect('/login');
-//     }
-//   });
-// });
-
+router.post('/add-broadcast', getAuthorization, AuthController.addBroadcast)
 router.get('/logout', (req,res) => {
   res.clearCookie('authToken'); // Hapus cookie authToken
     res.redirect('/login'); // Arahkan pengguna ke halaman login
 })
-
-router.post('/save-app-password', getAuthorization, validationMiddleware(AppPasswordDTO), AuthController.saveAppPassword);
 
 
 export default router;
