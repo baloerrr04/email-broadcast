87a57fc88255f7417f04d88ba4fd8d6fcfc46218
baloerrr <akbaradityah444@gmail.com>
2024-09-12 19:08:49 +0700

    refactor: restructuring code

--- src/modules/mailer/mailer.routes.ts
+++ src/modules/mailer/mailer.routes.ts
@@ -4,11 +4,8 @@ import { getAuthorization } from '../auth/auth.middleware';
 
 const router = Router();
 
-router.get('/emails', getAuthorization, MailController.getEmails);
-router.post('/send-email', MailController.sendEmail);
-// router.post('/emails/:id/edit', MailController.updateEmail);
+router.post('/send-email', getAuthorization, MailController.sendEmail);
 router.delete('/emails/:id', getAuthorization, MailController.deleteEmail);
-
 
 
 export default router;
