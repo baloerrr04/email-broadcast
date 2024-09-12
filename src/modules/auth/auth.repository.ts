aa5ab32c75a7423d552a93e4e0ca3936c0eaa555
baloerrr <akbaradityah444@gmail.com>
2024-09-12 19:05:22 +0700

    feat: add repo findByEmailBroadcaster

--- src/modules/auth/auth.repository.ts
+++ src/modules/auth/auth.repository.ts
@@ -1,3 +1,4 @@
+import { Broadcaster, Role } from "@prisma/client";
 import { User } from "../../common/types/userTypes";
 import { prisma } from "../../config/database";
 
@@ -5,29 +6,46 @@ class AuthRepository {
     static async findByEmail(email: string): Promise<User | null> {
         return prisma.user.findUnique({
             where: { email },
-        });
+        }) as unknown as User;
     }
 
     static async findById(id: number): Promise<User | null> {
         return prisma.user.findUnique({
             where: { id }
+        }) as unknown as User;
+    }
+
+    static async createUser(user: { email: string, password: string, role: Role }): Promise<User | null> {
+        return prisma.user.create({
+            data: {
+                ...user,
+                role: user.role
+            },
+        }) as unknown as User;
+    }
+
+    static async createBroadcaster(broadcaster: {email: string, appPassword:string}): Promise<Broadcaster | null> {
+        return prisma.broadcaster.create({
+            data: {
+                ...broadcaster
+            }
         })
     }
 
-    static async createUser(user: { email: string, password: string }): Promise<User | null> {
-        return prisma.user.create({
-            data: user
+    static async findByEmailBroadcaster(email: string): Promise<Broadcaster | null> {
+        return prisma.broadcaster.findUnique({
+            where: {email}
         })
     }
     
-    static async updateUser(id: number, appPassword: string): Promise<User | null> {
-        return prisma.user.update({
-            where: { id },
-            data: {
-                appPassword: appPassword
-            }
-        });
-    }
+    // static async updateUser(id: number, appPassword: string): Promise<User | null> {
+    //     return prisma.user.update({
+    //         where: { id },
+    //         data: {
+    //             appPassword: appPassword
+    //         }
+    //     });
+    // }
 }
 
 export default AuthRepository
