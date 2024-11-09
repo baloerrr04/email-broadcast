a0557747fee14e19b6b84c8b4e8074ac13a1f60b
baloerrr <akbaradityah444@gmail.com>
2024-11-09 23:12:13 +0700

    chore: delete route api register

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -2,11 +2,9 @@ import { Router } from 'express';
 import { getAuthorization, validationMiddleware } from '../../middlewares/auth.middleware';
 import AuthController from './auth.controller';
 import { LoginDTO } from './dtos/login.dto';
-import { RegisterDTO } from './dtos/register.dto';
 
 const router = Router();
 
-router.post('/register',validationMiddleware(RegisterDTO), AuthController.register);
 router.post('/login', validationMiddleware(LoginDTO), AuthController.login);
 router.post('/add-broadcast', getAuthorization, AuthController.addBroadcast)
 router.get('/logout', (req,res) => {
