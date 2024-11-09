7a86a82350fa331c0f8789e3334b8e842982cf80
baloerrr <akbaradityah444@gmail.com>
2024-11-09 23:08:04 +0700

    add: api route edit email

--- src/modules/mailer/mailer.view.route.ts
+++ src/modules/mailer/mailer.view.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { getAuthorization } from '../../middlewares/auth.middleware';
+import { authorizeRole, getAuthorization } from '../../middlewares/auth.middleware';
 import MailerViewController from './mailer.view.controller';
 
 const router = Router();
 
-router.get('/', getAuthorization, MailerViewController.sentMailPage);
-router.get('/scheduled', getAuthorization, MailerViewController.scheduledMailPage);
-router.get('/send-email', getAuthorization, MailerViewController.sendMailPage);
+router.get('/', getAuthorization, authorizeRole('USER'), MailerViewController.sentMailPage);
+router.get('/scheduled', getAuthorization, authorizeRole('USER'), MailerViewController.scheduledMailPage);
+router.get('/send-email', getAuthorization, authorizeRole('USER'), MailerViewController.sendMailPage);
+router.get('/emails/:id/edit', getAuthorization, authorizeRole('USER'), MailerViewController.editMailPage);
 
 export default router;
