5c48f88445c7c821dc6e44c1e8a88c0666dc6d9c
baloerrr <akbaradityah444@gmail.com>
2024-11-09 23:10:01 +0700

    chore: delete unused code

--- src/modules/mailer/mailer.controller.ts
+++ src/modules/mailer/mailer.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import MailService from './mailer.service';
 import AuthRepository from '../auth/auth.repository';
+import AdminRepository from '../admin/admin.repository';
 import moment from 'moment';
 import cron from "node-cron";
 import { HttpException } from '../../common/exceptions/HttpExceptions';
@@ -12,33 +13,11 @@ import HttpStatusCodes from '../../common/constants/http-status-codes';
 
 class MailController {
 
-    // static async getEmailsByStatus(req: Request, res: Response) {
-    //     try {
-    //         const userId = (req.user as { id: number }).id;
-    //         if (userId === undefined) {
-    //             return res.status(400).json({ message: 'User ID is not available' });
-    //         }
-    //         const status = req.params.status;
-    //         const emails = await MailRepository.findByUserIdAndStatus(userId, status);
-        
-    //         if (status === "Terkirim") {
-    //             res.render('index.ejs', { emails, user: req.user, title: 'Broadcast' });
-    //         } else if (status === "Menunggu") {
-    //             res.render('scheduled.ejs', { emails, user: req.user, title: 'Scheduled Emails' });
-    //         } else {
-    //             res.status(400).json({ message: 'Invalid status' });
-    //         }
-    //     } catch (error: any) {
-    //         res.status(500).json({ message: error.message });
-    //     }
-    // }
-
-
     static async sendEmail(req: Request, res: Response) {
         try {
             const { userId, from, to, cc, bcc, subject, content, scheduleDate, scheduleTime } = req.body;
             const user = await AuthRepository.findById(parseInt(userId))
-            const broadcaster = await AuthRepository.findByEmailBroadcaster(from)
+            const broadcaster = await AdminRepository.findByEmailBroadcaster(from)
 
             if (!user) throw new HttpException(400, "Broadcaster not found or app password not set");
             if (!broadcaster || !broadcaster.appPassword) throw new HttpException(400, "Broadcaster not found or app password not set");
@@ -53,34 +32,34 @@ class MailController {
 
             const baseUrl = 'http://localhost:3000/';
 
-            const downloadImage = async (url: string, filename: string) => {
+            const downloadImage = async (url: string, filename: string, index: number) => {
                 try {
                     const response = await axios.get(url, { responseType: 'arraybuffer' });
                     return {
                         filename,
                         content: Buffer.from(response.data),
-                        cid: `image${new Date().getTime()}.nodemailer`
+                        cid: `image${index}.example.com`  // cid tetap berdasarkan indeks gambar
                     };
                 } catch (error) {
                     console.error('Error downloading image:', url, error);
                     throw error;
                 }
             };
-
+            
             const $ = Cheerio.load(content);
             const attachments: any[] = [];
-
+            
+            // Loop through all images to create attachments and cid references
             const promises = $('img').map(async (index, img) => {
                 let src = $(img).attr('src');
-                console.log('Image src:', src);
-
+                
                 if (src) {
+                    const filename = `image${index}.png`;
+                    const cid = `image${index}.example.com`;  // Gunakan format konsisten untuk cid
+            
                     if (src.startsWith('data:')) {
-                        console.log("src1: ", src);
                         const base64Data = src.split(';base64,').pop();
                         if (base64Data) {
-                            const filename = `image${index}.png`;
-                            const cid = `image${index}@${new Date().getTime()}.nodemailer`;
                             attachments.push({
                                 filename,
                                 content: Buffer.from(base64Data, 'base64'),
@@ -89,11 +68,10 @@ class MailController {
                             $(img).attr('src', `cid:${cid}`);
                         }
                     } else {
-                        src = baseUrl + src
-                        const filename = `image${index}.png`;
+                        src = baseUrl + src;
                         try {
                             console.log('Downloading image from:', src);
-                            const imageAttachment = await downloadImage(src, filename);
+                            const imageAttachment = await downloadImage(src, filename, index);
                             attachments.push(imageAttachment);
                             $(img).attr('src', `cid:${imageAttachment.cid}`);
                         } catch (error) {
@@ -104,7 +82,7 @@ class MailController {
                     console.error('Image src is missing');
                 }
             }).get();
-
+            
             await Promise.all(promises);
 
             const updatedContent = $.html();
@@ -138,6 +116,7 @@ class MailController {
             res.status(500).json({ message: error.message });
         }
     }
+
 
     static async deleteEmail(req: Request, res: Response) {
         try {
